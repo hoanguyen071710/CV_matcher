350e0fce921f1d6dec1fdec1f7feb6c10e3788e9
HoaNguyenTechX <hoa.nguyen@techxcorp.com>
2024-09-20 00:22:44 +0700

    Update main

--- python/src/CV_matcher/__main__.py
+++ python/src/CV_matcher/__main__.py
@@ -1,10 +1,13 @@
 import argparse
 
 from .controller.runserver import app
+from .service.jobScraper.TimeBased import TimeBasedScrape
 
 
 def run_daily_job():
-    pass
+    tc = TimeBasedScrape()
+    print("Start scraping ...")
+    tc.scrape()
 
 def run_server():
     import uvicorn
@@ -14,11 +17,12 @@ def run_server():
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--server", action="store_true")
-    parser.add_argument("--daily-job-crawl", action="store_true")
+    parser.add_argument("--timebased-crawl", action="store_true")
     args = parser.parse_args()
     if args.server:
         print("run server")
         run_server()
-    elif args.daily_job_crawl:
-        # run_daily_job()
-        print("run daily job")
+    elif args.timebased_crawl:
+        print("Start time-based scrape")
+        run_daily_job()
+        # print("run daily job")
